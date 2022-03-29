14cb7c2009c498a698eba0438bf23b12c593ca02
winny1212 <maomaodragon1212@gmail.com>
2022-03-29 21:53:43 +1100

    import circle progress

--- src/components/CoinListTable.js
+++ src/components/CoinListTable.js
@@ -2,10 +2,16 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import {
   Grid,
-  createTheme,
-  ThemeProvider,
+  TableCell,
+  CircularProgress,
   Typography,
   TextField,
+  TableBody,
+  TableRow,
+  TableHead,
+  TableContainer,
+  Table,
+  Paper,
 } from '@mui/material';
 import { CoinList } from '../agent';
 
@@ -23,32 +29,44 @@ const CoinListTable = () => {
     setLoading(false);
   };
 
-  const darkTheme = createTheme({
-    palette: {
-      primary: {
-        main: '#fff',
-      },
-      type: 'dark',
-    },
-  });
-
   return (
-    <ThemeProvider theme={darkTheme}>
-      <Grid style={{ textAlign: 'center' }}>
-        <Typography
-          variant='h4'
-          style={{ margin: 18, fontFamily: 'Montserrat' }}
-        >
-          Cryptocurrency Prices by Market Cap
-        </Typography>
-        <TextField
-          label='Search For a Crypto Currency..'
-          variant='outlined'
-          style={{ marginBottom: 20, width: '100%' }}
-          onChange={(e) => setSearch(e.target.value)}
-        />
-      </Grid>
-    </ThemeProvider>
+    <Grid style={{ textAlign: 'center' }}>
+      <Typography variant='h4' style={{ margin: 18, fontFamily: 'Montserrat' }}>
+        Cryptocurrency Prices by Market Cap
+      </Typography>
+      <TextField
+        label='Search For a Crypto Currency..'
+        variant='outlined'
+        style={{ marginBottom: 20, width: '100%' }}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <TableContainer component={Paper}>
+        {loading ? (
+          <CircularProgress color='success' />
+        ) : (
+          <Table aria-label='simple table'>
+            <TableHead style={{ backgroundColor: '#EEBC1D' }}>
+              <TableRow>
+                {['Coin', 'Price', '24h Change', 'Market Cap'].map((head) => (
+                  <TableCell
+                    style={{
+                      color: 'black',
+                      fontWeight: '700',
+                      fontFamily: 'Montserrat',
+                    }}
+                    key={head}
+                    align={head === 'Coin' ? '' : 'right'}
+                  >
+                    {head}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+          </Table>
+        )}
+      </TableContainer>
+    </Grid>
   );
 };
 

1181e1fdbc86e2450e713ee2655c2c1c3d749580
winny1212 <maomaodragon1212@gmail.com>
2022-03-29 20:25:55 +1100

    setup the initial state of coin list table and get the coin list from API

--- src/components/CoinListTable.js
+++ src/components/CoinListTable.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { CoinList } from '../agent';
 
 const CoinListTable = () => {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  const fetchCoins = async () => {
+    setLoading(true);
+    const { data } = await axios.get(CoinList);
+    console.log(data);
+
+    setCoins(data);
+    setLoading(false);
+  };
 
   return <div>Coinlist</div>;
 };
